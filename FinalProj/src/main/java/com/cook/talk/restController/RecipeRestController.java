f9dc8a66ece838b4c3c955e1fd899545bb6c9ebc
sydneykang <kkangjyung114@gmail.com>
2020-06-08 17:46:15 +0900

    17:46 jm

--- FinalProj/src/main/java/com/cook/talk/restController/RecipeRestController.java
+++ FinalProj/src/main/java/com/cook/talk/restController/RecipeRestController.java
@@ -1,43 +1,49 @@
 package com.cook.talk.restController;
 
-
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cook.talk.model.VO.IngrVO;
 import com.cook.talk.model.dao.RecipeDAO;
 import com.cook.talk.model.service.RecipeService;
+import com.cook.talk.model.serviceImpl.RecipeServiceImpl;
 
 @RestController
-@RequestMapping("/ingrSelect")
 public class RecipeRestController {
 
 	@Autowired(required = false)
 	RecipeDAO recipeDAO;
-	
+
 	@Autowired(required = false)
 	RecipeService recipeService;
-	
+
 	@GetMapping(value = "/chosung")
 	public List<String> chosung(IngrVO ingrVO, HttpSession session, int cs) {
-		
-		/*
-		 * if (ingrVO.getIngrName() != null) { // DB검색이 아닌 내가 검색한 키워드를 저장하여 이미 있는 리스트에서
-		 * 검색 model.addAttribute("ingrName", ingrVO.getIngrName()); }
-		 */
-		
-		//재료 리스트
+
+		// 재료 리스트
 		List<String> ingrs = recipeService.ingrNameList(cs);
-	
+
 		return ingrs;
-	
+
 	}
-	
+
+	@RequestMapping(value = "/searched", method = RequestMethod.POST)
+	public String searched(IngrVO ingrVO) {
+		
+		recipeService.searched(ingrVO);
+
+		ingrVO.getIngrName();
+		System.out.println("ingrVO" + ingrVO);
+		return recipeService.searched(ingrVO);
+	}
 
 }
--- FinalProj/src/main/resources/static/js/recipe.js
+++ FinalProj/src/main/resources/static/js/recipe.js
@@ -2,25 +2,37 @@ $(function(){
 	selectedIngr();
 	ingrSearch();
 	$('#ingrSearchInRefri').on('change', ingrSearch);
-	 $('#btnSearch').on('click',ingrSearch);
+	$('#btnSearch').on('click',ingrSearch);
+	enterkey(); /*도대체 여기는 왜자꾸 콘솔에서 에러 뜨냐구..*/
 })
 
-function ingrSearch(){
-		    var key = $('#ingrSearchInRefri').val();
-		    console.log("key : "+key);
-		   
-		    var ingrList = $('#ingrList > li');
+/*엔터쳤을 때 페이지이동 없이 처리하기 위해*/
+function enterkey() {
+	if (event.keyCode == 13) {
+		ingrSearch();
+		event.preventDefault();
+	};
+}
 
-		    $.each(ingrList, function(index, item){	
-		        var title = $(this).find(".tit-area .tit").text();
-		        console.log("title :  "+title);
-		       
-		        if( title.includes(key) ){
-		            $(this).show();
-		        }else{
-		            $(this).hide();
-		        }
-		    });
+function ingrSearch(){
+
+	var key = $('#ingrSearchInRefri').val();
+	console.log("key:"+key);
+	
+	$.ajax({
+		type: "POST",
+		url: "/searched",
+		data: {ingrName:key},
+		dataType : 'TEXT',
+		success : function(data){
+			console.log(data);
+			$('#searchedIngr').text(data);
+		},
+		error: function(){
+			alert("Error");
+		}
+	});
+	
 }
 
 function selectedIngr(){
