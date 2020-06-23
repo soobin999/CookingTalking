1c69f5a68ac151cb772cff3207eba5f34c70b2f9
sydneykang <kkangjyung114@gmail.com>
2020-06-23 10:08:58 +0900

    jmjm 10:08 from com

--- FinalProj/src/main/resources/static/js/mypage.js
+++ FinalProj/src/main/resources/static/js/mypage.js
@@ -5,12 +5,17 @@ $(function(){
 
 function answer(){
 	
-	var ans = $('#answer').val();
-	
-	if(ans != null){
-		$('#answer').append("답변완료");
-	} else 
-		$('#answer').append("답변미등록");
+
+	$('#qnaAns').each(function(index){
+
+		var ans = $('#qnaAns').val();
+		
+		if(ans != null){
+			$('#qnaAns').append("답변완료");
+		} else {
+			$('#qnaAns').append("답변미등록");
+		}
+	})
 }
 
 
@@ -308,11 +313,36 @@ function sendInq(){
 
 function showAns(){
 	
-	$('#myQnATitle').on('click', function(){
+	$('#answer').on('click', function(){
 		
-		var myQnACont = $('#myQnACont').innerText();
-		var answer = $('#answer').val();
+/*		var myQnACont = $('#myQnACont').innerText();
+		var answer = $('#answer').val();*/
 		
+		
+		$.ajax({
+			type : "POST",
+			url : "/mypage/ans/{qnaCode}",
+			data : {qnaCode : key},
+			success : function(data) {
+				var searchedAllCom;
+				var result = $('#myActivity');
+				/*$('#myAllComMain').remove();*/
+				$.each(data, function(index, value){
+					console.log(data);
+					searchedAllCom = '<div th:id="myAllComMain"><tr><td>' + data[index].talkCom
+					+ '</td><td>' + data[index].talkComDate
+					+ '</td></tr><br></div>'
+					console.log("data.talkCom:"+value.talkCom);
+					console.log("data.talkComDate:"+value.talkComDate);
+					result.append(searchedAllCom);
+				});
+			},
+			error : function(){
+				alert("myAllComSearch Error");
+			}
+		});
+		
+		$('#showAns').append("${myInqList.answer}");
 		
 	})
 }
